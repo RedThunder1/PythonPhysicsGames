ee1f22382c4e3751c8fa1f140d9d027faaded99e
James <redthunderspartan@gmail.com>
2024-01-17 22:21:51 -0600

    Did some changes. Math sucks.

--- Physics/BallPhysics.py
+++ Physics/BallPhysics.py
@@ -3,26 +3,29 @@ import math
 
 #Setup
 pygame.init()
-screen = pygame.display.set_mode((1920,1080))
-clock = pygame.time.Clock()
-running = True
-statText = pygame.font.SysFont('arial', 30)
+screen = pygame.display.set_mode((1920,1080)) #Set resolution for screen. Don't change this value as it will mess things up.
+clock = pygame.time.Clock() #Instaciates clock for the application.
+running = True #While true the app will run and when false the app will close.
+statText = pygame.font.SysFont('arial', 30) #Text Settings for all of the text displayed.
 
 #Physics variables
-gravity = 0.6 #Default is 0.6, Isn't finely tuned so this will likely change
-speed = pygame.Vector2(10, -10) #Speed of ball and the direction +x is right, +y is down
-angle = 0 #Angle of the balls speed 0 - 360, 0 being directly upwards and angle and is clockwise
-center = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2) #Center of screen, used for finding ball relative to the containing circle
+gravity = 0.6 #Default is 0.6, Isn't finely tuned so this will likely change.
+speed = pygame.Vector2(20, -5) #Speed of ball and the direction +x is right -x is left, +y is down -y is upwards. This is because the coordinate system starts at the top left.
+angle = 0 #Angle of the balls from the center of the circle, 0 - 359.
+center = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2) #Center of screen, used for finding ball relative to the center containing circle.
 
-#maxSpeed = 100 max speed for ball if wanted
-collided = False #Stops ball from bouncing more than once while colliding once
-radius = 500 #Radius of the containing circle
-energyLoss = 0.1 #lost energy on bounce, is 1.00 - 0.00, can be over 1 to make it gain energy
-waitDefault = 3 #Default value for wait
-wait = 3 #Frames to wait before turning off collided, Stops the ball from being stuck in the wall
+
+#maxSpeed = 100 max speed for ball if wanted. Currently not implemented.
+collided = False #Stops ball from bouncing more than once while colliding.
+radius = 500 #Radius of the containing circle. Can be freely changed but I would recommend to leave as is.
+energyLoss = 0.95 #lost energy on bounce, numbers less than 1 cause it to lose energy on bounce and numbers greater than one cause it to gain energy. Currently not implemented.
+waitDefault = 2 #Default value for wait, recommended to leave at 2.
+wait = 2 #Frames to wait before turning off collided, Stops the ball from being stuck in the wall.
+top = pygame.Vector2(screen.get_width() / 2, (screen.get_height() / 2) - radius) #Top of incasing circle, used for calculations.
 
 #Physics objects
-ballCoords = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2) #Ball coords on screen with [0,0] being top left
+ballCoords = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2) #Ball coords on screen with [0,0] being top left.
+centeredCoords = pygame.Vector2(0, 0) #Ball coordinates which are centered to the screen. Allows for easier calculations.
 
 while running:
     #Events
@@ -30,51 +33,45 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    #Fill screen
-    screen.fill("black")
+    
+    
 
-    #Render Frames
-    #Render Circles
-    pygame.draw.circle(screen, "white", pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2), radius+5)
-    pygame.draw.circle(screen, "black", pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2), radius)
-    pygame.draw.circle(screen, "white", ballCoords, 5)
+    #Math
+    ballDistance = ballCoords.distance_to(center) #Get ball's distance from center, Used for angle calculations and collisions.
 
-    #Render Text
-    speedSurface = statText.render(f"Speed: {round(math.sqrt((speed.x * speed.x) + (speed.y * speed.y)), 5)} {speed}", False, 'White')
-    positionSurface = statText.render(f"Position: {ballCoords}", False, 'White')
-    angleSurface = statText.render(f"Angle: {angle}", False, 'White')
-    screen.blit(speedSurface, (screen.get_width() / 1.2, screen.get_height() / 6))
-    screen.blit(positionSurface, (screen.get_width() / 1.2, screen.get_height() / 5))
-    screen.blit(angleSurface, (screen.get_width() / 1.2, screen.get_height() / 4.3))
+    #Get Angle
+    sideC = ballDistance
+    sideB = radius
+    sideA = ballCoords.distance_to(top)
 
-    #Flip to send frame
-    pygame.display.flip()
-
-    #Math for next frame
-    #Get Hypotenuse
-    hyp = math.sqrt(((ballCoords.x - center.x) * (ballCoords.x - center.x)) + ((ballCoords.y - center.y) * (ballCoords.y - center.y)))
-
-    #Get Angle (I want to find a easier way to do this but it errors if either value is 0 so the if checks stay for now)
-    if speed.x == 0:
-        if -speed.y <= 0:
+    
+    
+    if centeredCoords.x == 0:
+        if -centeredCoords.y <= 0:
             angle = 180
         else:
             angle = 0
-    elif -speed.y == 0:
-        if speed.x <= 0:
+    elif -centeredCoords.y == 0:
+        if centeredCoords.x <= 0:
             angle = 270
         else:
             angle = 90
+    elif -centeredCoords.y == 0 and centeredCoords.x == 0:
+        angle = 0
     else:
-        angle = math.tan(speed.y/speed.x)
+        try: #Bellow causes a domain error, but im not sure why. The formula is probably messed up.
+            angle = 1
+        except:
+            print("Angle Error")
+        
 
     #Calculate gravity and if colliding
-    
-    colliding = hyp >= radius
+    #Bounces are not correct yet, 
+    colliding = ballDistance >= radius
 
     if colliding and not collided:
         collided = True
-        speed *= -1 #Not at all correct bounces but I needed something simple to test collisions, Collisions aren't perfect but they'll work for now
+        speed *= -1
     elif collided:
         if wait == 0:
             wait = waitDefault
@@ -85,6 +82,39 @@ while running:
     ballCoords += speed
 
 
-    clock.tick(60) #Frames per second but it will mess with sim if changed so leave it as is, I may change this sometime
+    #Fill screen
+    screen.fill("black")
+
+    #Render Frames
+    #Render Circles and debug line
+    
+    pygame.draw.circle(screen, "white", center, radius+5)
+    pygame.draw.circle(screen, "black", center, radius)
+    pygame.draw.line(screen, "yellow", center, ballCoords, 3)
+    pygame.draw.circle(screen, "red", center, 4)
+    pygame.draw.circle(screen, "white", ballCoords, 5)
+
+    #Collision lines
+    
+    pygame.draw.line(screen, "orange", top, ballCoords)
+    pygame.draw.line(screen, "orange", center, top)
+
+    #Render Text
+    
+    centeredCoords = ballCoords - center #Centered coords for position.
+
+    speedSurface = statText.render(f"Speed: {round(math.sqrt((speed.x * speed.x) + (speed.y * speed.y)), 2)} {round(speed, 2)}", False, 'White')
+    positionSurface = statText.render(f"Position: [{centeredCoords.x}, {-centeredCoords.y}]", False, 'White')
+    angleSurface = statText.render(f"Angle: {angle}", False, 'White')    
+    sideSurface = statText.render(f"SideA: {round(sideA)} SideB: 500 SideC: {round(sideC)}", False, 'white')
+    screen.blit(speedSurface, (screen.get_width() / 1.2, screen.get_height() / 6))
+    screen.blit(positionSurface, (screen.get_width() / 1.2, screen.get_height() / 5))
+    screen.blit(angleSurface, (screen.get_width() / 1.2, screen.get_height() / 4.3))
+    screen.blit(sideSurface, (screen.get_width() / 1.5, screen.get_height() - 100))
+
+    #Flip to send frame
+    pygame.display.flip()
+
+    clock.tick(60) #Frames per second but it will mess with sim if changed so leave it as is, I may change this sometime.
 
 pygame.quit
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-A little project im doing in my free time where im making little physics sims using pygame. Curretly im making a sim for a ball bouncing inside a circle.
+# Python Physics Sims
+In this repository I will be creating little physics sims using pygame for visuals and math to simulate certain physics scenarios.
+
+The first and current sim is a ball bouncing around the inside of a containing circle.
+
+These sims aren't anything more than just little projects I wanted to make so they wont have any practical use (likely) and they will be far from perfect. Despite that I want to make them as realistic and efficent as possible. As I create more I may up the difficulty of the sims to help myself learn more. Anyways if you are here at this repository I hope you enjoy this dumb little sims.
