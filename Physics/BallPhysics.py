6219a8cf09621ff010d3f2004c349638e954b0a8
James <redthunderspartan@gmail.com>
2024-01-20 15:13:22 -0600

    Angle Works now, now to properly simulate bounces.

--- Physics/BallPhysics.py
+++ Physics/BallPhysics.py
@@ -6,6 +6,7 @@ pygame.init()
 screen = pygame.display.set_mode((1920,1080)) #Set resolution for screen. Don't change this value as it will mess things up.
 clock = pygame.time.Clock() #Instaciates clock for the application.
 running = True #While true the app will run and when false the app will close.
+paused = False #Allows sim to be paused
 statText = pygame.font.SysFont('arial', 30) #Text Settings for all of the text displayed.
 
 #Physics variables
@@ -32,85 +33,90 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_p]:
+                print("Paused")
+                paused = not paused
 
     
     
+    if not paused:
+        #Math
+        ballDistance = ballCoords.distance_to(center) #Get ball's distance from center, Used for angle calculations and collisions.
 
-    #Math
-    ballDistance = ballCoords.distance_to(center) #Get ball's distance from center, Used for angle calculations and collisions.
+        #Get Angle
+        sideC = ballDistance
+        sideB = radius
+        sideA = ballCoords.distance_to(top)
 
-    #Get Angle
-    sideC = ballDistance
-    sideB = radius
-    sideA = ballCoords.distance_to(top)
-
-    
-    
-    if centeredCoords.x == 0:
-        if -centeredCoords.y <= 0:
-            angle = 180
-        else:
-            angle = 0
-    elif -centeredCoords.y == 0:
-        if centeredCoords.x <= 0:
-            angle = 270
-        else:
-            angle = 90
-    elif -centeredCoords.y == 0 and centeredCoords.x == 0:
-        angle = 0
-    else:
-        try: #Bellow causes a domain error, but im not sure why. The formula is probably messed up.
-            angle = 1
-        except:
-            print("Angle Error")
         
-
-    #Calculate gravity and if colliding
-    #Bounces are not correct yet, 
-    colliding = ballDistance >= radius
-
-    if colliding and not collided:
-        collided = True
-        speed *= -1
-    elif collided:
-        if wait == 0:
-            wait = waitDefault
-            collided = False
+        
+        if centeredCoords.x == 0:
+            if -centeredCoords.y <= 0:
+                angle = 180
+            else:
+                angle = 0
+        elif -centeredCoords.y == 0:
+            if centeredCoords.x <= 0:
+                angle = 270
+            else:
+                angle = 90
+        elif -centeredCoords.y == 0 and centeredCoords.x == 0:
+            angle = 0
         else:
-            wait -= 1
-    speed.y += gravity
-    ballCoords += speed
+            angle = math.degrees(math.acos(((sideB * sideB) + (sideC * sideC) - (sideA * sideA))/(2 * sideB * sideC)))
+            if centeredCoords.x < 0:
+                angle = 360 - angle
+
+            
+
+        #Calculate gravity and if colliding
+        #Bounces are not correct yet, 
+        colliding = ballDistance >= radius
+
+        if colliding and not collided:
+            collided = True
+            speed *= -1
+        elif collided:
+            if wait == 0:
+                wait = waitDefault
+                collided = False
+            else:
+                wait -= 1
+        speed.y += gravity
+        ballCoords += speed
 
 
-    #Fill screen
-    screen.fill("black")
+        #Fill screen
+        screen.fill("black")
 
-    #Render Frames
-    #Render Circles and debug line
-    
-    pygame.draw.circle(screen, "white", center, radius+5)
-    pygame.draw.circle(screen, "black", center, radius)
-    pygame.draw.line(screen, "yellow", center, ballCoords, 3)
-    pygame.draw.circle(screen, "red", center, 4)
-    pygame.draw.circle(screen, "white", ballCoords, 5)
+        #Render Frames
+        #Render Circles and debug line
+        
+        pygame.draw.circle(screen, "white", center, radius+5)
+        pygame.draw.circle(screen, "black", center, radius)
+        pygame.draw.line(screen, "yellow", center, ballCoords, 3)
+        pygame.draw.circle(screen, "red", center, 4)
+        pygame.draw.circle(screen, "white", ballCoords, 5)
 
-    #Collision lines
-    
-    pygame.draw.line(screen, "orange", top, ballCoords)
-    pygame.draw.line(screen, "orange", center, top)
+        #Collision lines
+        
+        pygame.draw.line(screen, "orange", top, ballCoords)
+        pygame.draw.line(screen, "orange", center, top)
 
-    #Render Text
-    
-    centeredCoords = ballCoords - center #Centered coords for position.
+        #Render Text
+        
+        centeredCoords = ballCoords - center #Centered coords for position.
 
-    speedSurface = statText.render(f"Speed: {round(math.sqrt((speed.x * speed.x) + (speed.y * speed.y)), 2)} {round(speed, 2)}", False, 'White')
-    positionSurface = statText.render(f"Position: [{centeredCoords.x}, {-centeredCoords.y}]", False, 'White')
-    angleSurface = statText.render(f"Angle: {angle}", False, 'White')    
-    sideSurface = statText.render(f"SideA: {round(sideA)} SideB: 500 SideC: {round(sideC)}", False, 'white')
-    screen.blit(speedSurface, (screen.get_width() / 1.2, screen.get_height() / 6))
-    screen.blit(positionSurface, (screen.get_width() / 1.2, screen.get_height() / 5))
-    screen.blit(angleSurface, (screen.get_width() / 1.2, screen.get_height() / 4.3))
-    screen.blit(sideSurface, (screen.get_width() / 1.5, screen.get_height() - 100))
+        speedSurface = statText.render(f"Speed: {round(math.sqrt((speed.x * speed.x) + (speed.y * speed.y)), 2)} {round(speed, 2)}", False, 'White')
+        positionSurface = statText.render(f"Position: [{centeredCoords.x}, {-centeredCoords.y}]", False, 'White')
+        angleSurface = statText.render(f"Angle: {angle}", False, 'White')    
+        sideSurface = statText.render(f"SideA: {round(sideA)} SideB: 500 SideC: {round(sideC)}", False, 'white')
+        screen.blit(speedSurface, (screen.get_width() / 1.2, screen.get_height() / 6))
+        screen.blit(positionSurface, (screen.get_width() / 1.2, screen.get_height() / 5))
+        screen.blit(angleSurface, (screen.get_width() / 1.2, screen.get_height() / 4.3))
+        screen.blit(sideSurface, (screen.get_width() / 1.5, screen.get_height() - 100))
 
     #Flip to send frame
     pygame.display.flip()
