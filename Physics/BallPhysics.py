fe8a83e8adea7857ec9cca9c3a72c517589545fd
James <redthunderspartan@gmail.com>
2024-01-23 07:46:39 -0600

    Did some work

--- Physics/BallPhysics.py
+++ Physics/BallPhysics.py
@@ -6,12 +6,13 @@ pygame.init()
 screen = pygame.display.set_mode((1920,1080)) #Set resolution for screen. Don't change this value as it will mess things up.
 clock = pygame.time.Clock() #Instaciates clock for the application.
 running = True #While true the app will run and when false the app will close.
-paused = False #Allows sim to be paused
+paused = False #Allows sim to be paused.
 statText = pygame.font.SysFont('arial', 30) #Text Settings for all of the text displayed.
 
 #Physics variables
 gravity = 0.6 #Default is 0.6, Isn't finely tuned so this will likely change.
 speed = pygame.Vector2(20, -5) #Speed of ball and the direction +x is right -x is left, +y is down -y is upwards. This is because the coordinate system starts at the top left.
+speedAngle = 0 #Gets angle of speed.
 angle = 0 #Angle of the balls from the center of the circle, 0 - 359.
 center = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2) #Center of screen, used for finding ball relative to the center containing circle.
 
@@ -19,7 +20,7 @@ center = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2) #Center
 #maxSpeed = 100 max speed for ball if wanted. Currently not implemented.
 collided = False #Stops ball from bouncing more than once while colliding.
 radius = 500 #Radius of the containing circle. Can be freely changed but I would recommend to leave as is.
-energyLoss = 0.95 #lost energy on bounce, numbers less than 1 cause it to lose energy on bounce and numbers greater than one cause it to gain energy. Currently not implemented.
+energyLoss = 0.95 #lost energy on bounce, numbers less than 1 cause it to lose energy on bounce and numbers greater than one cause it to gain energy.
 waitDefault = 2 #Default value for wait, recommended to leave at 2.
 wait = 2 #Frames to wait before turning off collided, Stops the ball from being stuck in the wall.
 top = pygame.Vector2(screen.get_width() / 2, (screen.get_height() / 2) - radius) #Top of incasing circle, used for calculations.
@@ -30,28 +31,23 @@ centeredCoords = pygame.Vector2(0, 0) #Ball coordinates which are centered to th
 
 while running:
     #Events
-    for event in pygame.event.get():
+    for event in pygame.event.get(): #Quit app when Quit putton is pressed
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN: #Pause Sim when p key is pressed.
             keys = pygame.key.get_pressed()
             if keys[pygame.K_p]:
-                print("Paused")
                 paused = not paused
-
-    
     
     if not paused:
         #Math
         ballDistance = ballCoords.distance_to(center) #Get ball's distance from center, Used for angle calculations and collisions.
 
-        #Get Angle
+        #Gets the angle around the center point, 0 - 359.
         sideC = ballDistance
         sideB = radius
         sideA = ballCoords.distance_to(top)
 
-        
-        
         if centeredCoords.x == 0:
             if -centeredCoords.y <= 0:
                 angle = 180
@@ -68,25 +64,29 @@ while running:
             angle = math.degrees(math.acos(((sideB * sideB) + (sideC * sideC) - (sideA * sideA))/(2 * sideB * sideC)))
             if centeredCoords.x < 0:
                 angle = 360 - angle
+        
+        #Get angle of speed
+        hyp = math.sqrt((speed.x * speed.x) + (speed.y * speed.y))
+        speedAngle = math.degrees(math.tan(speed.y/speed.x))
 
             
 
-        #Calculate gravity and if colliding
-        #Bounces are not correct yet, 
+        #Calculate gravity and if colliding.
+        #Bounces are not correct yet.
         colliding = ballDistance >= radius
-
+        
         if colliding and not collided:
             collided = True
-            speed *= -1
+            speed *= -energyLoss
         elif collided:
             if wait == 0:
                 wait = waitDefault
                 collided = False
             else:
                 wait -= 1
-        speed.y += gravity
+        
         ballCoords += speed
-
+        speed.y += gravity
 
         #Fill screen
         screen.fill("black")
@@ -96,22 +96,21 @@ while running:
         
         pygame.draw.circle(screen, "white", center, radius+5)
         pygame.draw.circle(screen, "black", center, radius)
-        pygame.draw.line(screen, "yellow", center, ballCoords, 3)
-        pygame.draw.circle(screen, "red", center, 4)
+        #pygame.draw.line(screen, "yellow", center, ballCoords, 3)
+        #pygame.draw.circle(screen, "red", center, 4)
+        pygame.draw.line(screen, "Blue", ballCoords, (ballCoords.x, ballCoords.y + 4 * speed.y), 5)
+        pygame.draw.line(screen, "Red", ballCoords, (ballCoords.x + 4 * speed.x, ballCoords.y), 5)
         pygame.draw.circle(screen, "white", ballCoords, 5)
 
-        #Collision lines
         
-        pygame.draw.line(screen, "orange", top, ballCoords)
-        pygame.draw.line(screen, "orange", center, top)
 
         #Render Text
         
         centeredCoords = ballCoords - center #Centered coords for position.
 
-        speedSurface = statText.render(f"Speed: {round(math.sqrt((speed.x * speed.x) + (speed.y * speed.y)), 2)} {round(speed, 2)}", False, 'White')
-        positionSurface = statText.render(f"Position: [{centeredCoords.x}, {-centeredCoords.y}]", False, 'White')
-        angleSurface = statText.render(f"Angle: {angle}", False, 'White')    
+        speedSurface = statText.render(f"Speed: {round(speedAngle, 2)} {round(speed, 2)}", False, 'White')
+        positionSurface = statText.render(f"Position: [{round(centeredCoords.x, 2)}, {round(-centeredCoords.y, 2)}]", False, 'White')
+        angleSurface = statText.render(f"Angle: {round(angle, 2)}", False, 'White')    
         sideSurface = statText.render(f"SideA: {round(sideA)} SideB: 500 SideC: {round(sideC)}", False, 'white')
         screen.blit(speedSurface, (screen.get_width() / 1.3, screen.get_height() / 6))
         screen.blit(positionSurface, (screen.get_width() / 1.3, screen.get_height() / 5))
